8d59179e0b3649646d3aff13a4cbd6c591b6201c
asupriana <alexa9410@gmail.com>
2019-02-05 13:23:27 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+//Variables
+const submitButton = document.querySelector(".form-submit");
+const signUpButton = document.querySelectorAll(".btn");
+const signUpPressure = document.querySelectorAll("h4");
+const comeBackHere = document.querySelector(".destination");
+const titleChange = document.querySelector("h1");
+const headerChange = document.querySelector(".main-navigation");
+const textChange = document.querySelector(".home");
+const navTags = document.querySelectorAll("a");
