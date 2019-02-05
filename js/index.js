f3857f3322c1b44353bbced5a7ae1007494e8392
asupriana <alexa9410@gmail.com>
2019-02-05 14:41:15 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,10 +2,29 @@
 
 //Variables
 const submitButton = document.querySelector(".form-submit");
-const signUpButton = document.querySelectorAll(".btn");
-const signUpPressure = document.querySelectorAll("h4");
+ const signUpButton = document.querySelectorAll(".btn");
+ const signUpPressure = document.querySelectorAll("h4");
 const comeBackHere = document.querySelector(".destination");
 const titleChange = document.querySelector("h1");
 const headerChange = document.querySelector(".main-navigation");
 const textChange = document.querySelector(".home");
 const navTags = document.querySelectorAll("a");
+
+signUpPressure.forEach ( pressure => {
+    pressure.addEventListener('mouseover', e =>{
+        window.alert("Sign up before time runs out! Double click now!");
+        e.stopPropagation();
+        });
+    });
+    
+signUpButton.forEach ( signUp => {
+    signUp.addEventListener('mouseenter', e =>{
+    e.target.style.backgroundColor = "orange";
+    });
+});
+
+signUpButton.forEach ( signUp => {
+    signUp.addEventListener('dblclick', e =>{
+        window.alert("Thank you for trying, CLICK AGAIN!");
+        });
+    });
